f42c9988969ae3019e081b4b73759c138c4d87be
eduardonk9999 <thiago.novak7@gmail.com>
2021-03-26 15:13:05 -0300

    Add, home

---
+++ dashbord/src/views/Home/CustomHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="flex flex-col w-4/5 max-w-6xl">
+      <div class="w-28 lg:w-36">
+
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss" scoped>
+  .header {
+    @apply bg-brand-main w-full flex justify-center;
+    height: 700px;
+  }
+</style>
--- dashbord/src/views/Home/index.vue
+++ dashbord/src/views/Home/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <h1 class="text-brand-main">Home</h1>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="hanfleLogin"
+   />
+  <contact />
+  <footer class="flex justify-center py-10 bg-brad-gray">
+    <p class="font-medim text-center text-gray-800">feedbacker 2021</p>
+  </footer>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
 
+export default {
+  components: {
+    CustomHeader,
+    Contact
+  },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedback' })
+      }
+    })
+    function hanfleLogin () {}
+    function handleAccountCreate () {}
+    return {
+      hanfleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
